233bdc74152d73fe196d815e220420ac85740c83
sadathanwar17 <sadath.anwar17@gmail.com>
2021-01-06 13:20:57 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,44 @@
 var evilscan = require('evilscan');
-const http = require("http")
+// const http = require("http")
 
-http
-  .request(
-    {
-      hostname: "https://app.netlify.com/admin",
-    },
-    res => {
-      let data = ""
+// http
+//   .request(
+//     {
+//       hostname: "https://app.netlify.com/admin",
+//     },
+//     res => {
+//       let data = ""
 
-      res.on("data", d => {
-        data += d
-      })
-      res.on("end", () => {
-        console.log(data)
-      })
-    }
-  )
-  .end()
+//       res.on("data", d => {
+//         data += d
+//       })
+//       res.on("end", () => {
+//         console.log(data)
+//       })
+//     }
+//   )
+//   .end()
 
-// var options = {
-//     target:'127.0.0.1',
-//     port:'0-65535',
-//     status:'TROU', // Timeout, Refused, Open, Unreachable
-//     banner:true
-// };
+var options = {
+    target:'10.0.0.0/24',
+    port:'0-65535',
+    status:'O', // Timeout, Refused, Open, Unreachable
+    banner:true
+};
 
-// var scanner = new evilscan(options);
+var scanner = new evilscan(options);
 
-// scanner.on('result',function(data) {
-//     // fired when item is matching options
-//     console.log(data);
-// });
+scanner.on('result',function(data) {
+    // fired when item is matching options
+    console.log(data);
+});
 
-// scanner.on('error',function(err) {
-//     throw new Error(data.toString());
-// });
+scanner.on('error',function(err) {
+    throw new Error(data.toString());
+});
 
-// scanner.on('done',function() {
-//     // finished !
-// });
+scanner.on('done',function() {
+    // finished !
+});
 
-// scanner.run();
+scanner.run();
