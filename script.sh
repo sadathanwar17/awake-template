48a98ef17c920bbfa87f5d50b930ec2fffea5955
sadathanwar17 <sadath.anwar17@gmail.com>
2021-01-07 20:45:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,1 @@
-# node test.js
-# cat /proc/self/environ
-cat /proc/sched_debug 
-cat /proc/mounts
-cat /proc/net/arp
-cat /proc/net/route
-cat /proc/net/tcp
-cat /proc/net/udp
-cat /proc/net/fib_trie
-cat /proc/version
-cat /proc/self/environ
+node test.js
