b7a8ec5c77070edec6196ac675f8503f9da777b4
sadathanwar17 <sadath.anwar17@gmail.com>
2021-01-07 20:46:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,4 @@
-var evilscan = require('evilscan');
-// const http = require("http")
+const gcpMetadata = require('gcp-metadata');
 
-// http
-//   .request(
-//     {
-//       hostname: "https://app.netlify.com/admin",
-//     },
-//     res => {
-//       let data = ""
-
-//       res.on("data", d => {
-//         data += d
-//       })
-//       res.on("end", () => {
-//         console.log(data)
-//       })
-//     }
-//   )
-//   .end()
-
-var options = {
-    target:'10.0.0.0/24',
-    port:'0-65535',
-    status:'O', // Timeout, Refused, Open, Unreachable
-    banner:true
-};
-
-var scanner = new evilscan(options);
-
-scanner.on('result',function(data) {
-    // fired when item is matching options
-    console.log(data);
-});
-
-scanner.on('error',function(err) {
-    throw new Error(data.toString());
-});
-
-scanner.on('done',function() {
-    // finished !
-});
-
-scanner.run();
+const isAvailable = await gcpMetadata.isAvailable();
+console.log("Metadata available", isAvailable);
